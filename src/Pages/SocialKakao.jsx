f4dfc40ae0661be811db2d490fb49eee325e219a
gyozaa01 <olivia110321@gmail.com>
2024-10-21 21:32:09 +0900

    feat: SocialKakao.jsx에도 주소 변경

--- src/Pages/SocialKakao.jsx
+++ src/Pages/SocialKakao.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SocialKakao = () => {
   const javascriptKey = process.env.REACT_APP_KAKAO_JS_KEY;
-  const redirect_uri = 'https://tb-mong-fe.vercel.app/auth'; // 인가 코드 처리할 페이지
+  const redirect_uri = 'https://tb-mong-fe.vercel.app/dongene-setting'; // 인가 코드 처리할 페이지
 
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${javascriptKey}&redirect_uri=${redirect_uri}&response_type=code`;
 
