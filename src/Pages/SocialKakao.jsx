f38edb3adc9ed6e0c350c5f221b806b4cb72aed8
gyozaa01 <olivia110321@gmail.com>
2024-10-23 23:35:04 +0900

    feat: 로컬과 배포 환경에 따른 redirect 설정

--- src/Pages/SocialKakao.jsx
+++ src/Pages/SocialKakao.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 const SocialKakao = () => {
   // REST API 키를 환경 변수에서 가져옴
-  const restApiKey = process.env.REACT_APP_KAKAO_REST_API_KEY; // REST API 키
-  const redirect_uri = 'https://tb-mong-fe.vercel.app/auth'; // 인가 코드 처리할 페이지
+  const restApiKey = process.env.REACT_APP_KAKAO_REST_API_KEY;
+
+  // 로컬과 배포 환경에 따라 redirect_uri를 설정
+  const redirectUri = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/auth' // 로컬 환경에서 사용될 URI
+    : 'https://tb-mong-fe.vercel.app/auth'; // 배포 환경에서 사용될 URI
 
   // Kakao 로그인 URL 생성 (REST API 키를 client_id로 사용)
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?&response_type=code&client_id=${restApiKey}&redirect_uri=${redirect_uri}`;
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?&response_type=code&client_id=${restApiKey}&redirect_uri=${redirectUri}`;
 
   const handleLogin = () => {
     if (!restApiKey) {
