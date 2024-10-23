c3bf7084bc33f0231dede177cb5178d998b0774d
gyozaa01 <olivia110321@gmail.com>
2024-10-23 23:20:13 +0900

    feat: socialkakao.jsx url 변경

--- src/Pages/SocialKakao.jsx
+++ src/Pages/SocialKakao.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const SocialKakao = () => {
-  const javascriptKey = process.env.REACT_APP_KAKAO_JS_KEY;
+  const javascriptKey = process.env.REACT_APP_KAKAO_REST_API_KEY;
   const redirect_uri = 'https://tb-mong-fe.vercel.app/auth'; // 인가 코드 처리할 페이지
 
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${javascriptKey}&redirect_uri=${redirect_uri}&response_type=code`;
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?&response_type=code&client_id=${javascriptKey}&redirect_uri=${redirect_uri}`;
 
   const handleLogin = () => {
     window.location.href = kakaoURL; // 카카오 로그인 페이지로 리디렉트
