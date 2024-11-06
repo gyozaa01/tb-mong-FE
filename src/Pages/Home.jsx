11f2b113faa5d2bbc3cb0a1237eca6eb2a1defff
gyozaa01 <olivia110321@gmail.com>
2024-11-06 23:02:18 +0900

    feat: home에 경험치 추가

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,91 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 import BottomNav from '../Components/BottomNav';
 import Header from '../Components/Header';
 
 const WRAPPER_WIDTH = '375px';
 
+const Home = () => {
+    const [level, setLevel] = useState(1);
+    const [experience, setExperience] = useState(3); // 테스트용으로 3으로 설정
+    const [showModal, setShowModal] = useState(false);
+
+    const confirmLevelUp = () => {
+        setLevel(level + 1);
+        setExperience(0);
+        setShowModal(false);
+    };
+
+    const handleBalloonClick = () => {
+        setShowModal(true);
+    };
+
+    return (
+        <Container>
+            <AppWrapper>
+                <Header />
+
+                <Stats>
+                    <StatItem>
+                        <img src="/b1.png" alt="Today" />
+                        <span>TODAY</span>
+                    </StatItem>
+                    <StatItem>
+                        <img src="/b4.png" alt="KM" />
+                        <span>KM</span>
+                    </StatItem>
+                    <StatItem>
+                        <img src="/b3.png" alt="Total" />
+                        <span>TOTAL</span>
+                    </StatItem>
+                    <StatItem>
+                        <img src="/b2.png" alt="Total" />
+                        <span>TOTAL KM</span>
+                    </StatItem>
+                </Stats>
+
+                <MainContent>
+                    <CharacterContainer>
+                        <img src="/mong1.png" alt="메인 캐릭터" className="mong" />
+                        {experience === 3 && (
+                            <Balloon onClick={handleBalloonClick}>
+                                <Exclamation>!</Exclamation>
+                            </Balloon>
+                        )}
+                        <img src="/bottom.png" alt="카펫" className="carpet" />
+                    </CharacterContainer>
+
+                    <LevelInfo>
+                        <span>Lv.{level}</span>
+                        <ExperienceImage src={`/exp${experience}.png`} alt={`Experience ${experience}`} />
+                    </LevelInfo>
+                </MainContent>
+
+                <Modal $show={showModal}>
+                    <Stars>
+                        <img src="/star.png" alt="Star" />
+                        <StarOffset src="/star.png" alt="Star" />
+                        <img src="/star.png" alt="Star" />
+                    </Stars>
+                    <LevelUpText>LEVEL UP</LevelUpText>
+                    <ButtonImage onClick={confirmLevelUp}>
+                        <span>OK</span>
+                    </ButtonImage>
+                </Modal>
+
+                <BottomNav />
+            </AppWrapper>
+        </Container>
+    );
+};
+
 const Container = styled.div`
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
     height: 100vh;
     background-color: #FEFEFE;
+    margin: 0;
 `;
 
 const AppWrapper = styled.div`
@@ -18,7 +93,7 @@ const AppWrapper = styled.div`
     max-width: ${WRAPPER_WIDTH};
     min-height: 100vh;
     background-color: #A7D2FF;
-    padding: 20px;
+    padding: 0 20px 20px;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -27,14 +102,44 @@ const AppWrapper = styled.div`
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
+const Stats = styled.div`
+    align-self: flex-start;
+    margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`;
+
+const StatItem = styled.div`
+    display: flex;
+    align-items: center;
+    font-size: 15px;
+    font-weight: bold;
+    color: black;
+    margin-bottom: 2px;
+
+    img {
+        width: 30px;
+        height: 30px;
+        margin-right: 5px;
+    }
+`;
+
 const MainContent = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
     flex-grow: 1;
-    position: relative;
+    justify-content: center;
+    margin-top: -30%;
+`;
 
+const CharacterContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    
     .mong {
         width: 150px;
         height: auto;
@@ -49,21 +154,112 @@ const MainContent = styled.div`
     }
 `;
 
-const Home = () => {
-    return (
-        <Container>
-            <AppWrapper>
-                <Header />
+const bounce = keyframes`
+    0%, 100% {
+        transform: translateY(0);
+    }
+    50% {
+        transform: translateY(-10px);
+    }
+`;
 
-                <MainContent>
-                    <img src="/mong1.png" alt="메인 캐릭터" className="mong" />
-                    <img src="/bottom.png" alt="카펫" className="carpet" />
-                </MainContent>
+const Balloon = styled.div`
+    position: absolute;
+    top: -40px;
+    background-image: url('/nemo.png');
+    background-size: cover;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    z-index: 3;
+    animation: ${bounce} 1s infinite;
+`;
 
-                <BottomNav />
-            </AppWrapper>
-        </Container>
-    );
-};
+const Exclamation = styled.span`
+    font-size: 20px;
+    font-weight: bold;
+    color: black;
+    margin-top: -2px;
+    font-family: 'DNFBitBitv2';
+`;
+
+const LevelInfo = styled.div`
+    display: flex;
+    align-items: center;
+    margin-top: -20px;
+`;
+
+const ExperienceImage = styled.img`
+    width: 200px;
+    height: 90px;
+    margin-left: 20px;
+    margin-top: -5px;
+`;
+
+const Modal = styled.div`
+    display: ${({ $show }) => ($show ? 'flex' : 'none')};
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    flex-direction: column;
+    align-items: center;
+    background-color: white;
+    padding: 40px;
+    border-radius: 10px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    width: 250px;
+    text-align: center;
+    z-index: 1000;
+`;
+
+const Stars = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 10px;
+    position: relative;
+
+    img {
+        width: 70px;
+        height: 70px;
+        margin: 0 5px;
+    }
+`;
+
+const StarOffset = styled.img`
+    width: 70px;
+    height: 70px;
+    margin: 0 1px;
+    position: relative;
+    top: -30px;
+`;
+
+const LevelUpText = styled.h2`
+    font-size: 35px;
+    margin-bottom: 20px;
+`;
+
+const ButtonImage = styled.button`
+    width: 200px;
+    height: 80px;
+    background-image: url('/setting_button.png');
+    background-size: cover;
+    background-position: center;
+    background-color: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+
+    span {
+        font-size: 16px;
+        color: black;
+        font-family: 'DNFBitBitv2';
+    }
+`;
 
 export default Home;
