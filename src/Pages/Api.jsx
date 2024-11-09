1eac1d831dcc523c248dcbdc595b07daee11bddf
gyozaa01 <olivia110321@gmail.com>
2024-11-09 20:02:43 +0900

    feat: api.jsx 8080으로 바꾸기

--- src/Pages/Api.jsx
+++ src/Pages/Api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Axios 인스턴스를 생성하여 기본 URL 설정
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
