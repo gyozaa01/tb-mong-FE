ecec1436ef69698c012c25589505d169ed48acba
gyozaa01 <olivia110321@gmail.com>
2024-11-09 21:21:05 +0900

    feat: api.jsx에 요청 인터셉터 추가

--- src/Pages/Api.jsx
+++ src/Pages/Api.jsx
@@ -1,12 +1,20 @@
 import axios from 'axios';
 
-// Axios 인스턴스를 생성하여 기본 URL 설정
 const api = axios.create({
   baseURL: 'http://localhost:8080',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  withCredentials: true, // 세션 쿠키를 포함하기 위해 설정
+  withCredentials: true,
 });
+
+// 요청 인터셉터
+api.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem('jwt_token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
