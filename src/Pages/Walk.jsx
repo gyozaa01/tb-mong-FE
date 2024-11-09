ddf183980a197cd5ca53d0eb38699f85ef7cd546
gyozaa01 <olivia110321@gmail.com>
2024-11-09 16:18:12 +0900

    feat: 캔버스 렌더링 지연 주기

--- src/Pages/Walk.jsx
+++ src/Pages/Walk.jsx
@@ -139,36 +139,38 @@ const Walk = () => {
 
     // Polyline 경로를 캔버스에 그리기 및 캡처
     const captureCanvasPolyline = () => {
-        console.log("Capture function called"); // 함수 실행 확인 로그
-        const canvas = canvasRef.current;
-        if (!canvas) {
-            console.error("Canvas not found");
-            return;
-        }
-        const ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-        ctx.beginPath();
-        polylinePath.forEach((point, index) => {
-            const { Ma, La } = point;
-            if (index === 0) {
-                ctx.moveTo(La, Ma);
-            } else {
-                ctx.lineTo(La, Ma);
+        setTimeout(() => { // 약간의 지연을 주어 캔버스가 렌더링되도록 보장
+            console.log("Capture function called"); // 함수 실행 확인 로그
+            const canvas = canvasRef.current;
+            if (!canvas) {
+                console.error("Canvas not found");
+                return;
             }
-        });
-        ctx.strokeStyle = "#FF0000";
-        ctx.lineWidth = 2;
-        ctx.stroke();
-    
-        html2canvas(canvas, { useCORS: true }).then((canvas) => {
-            const imageData = canvas.toDataURL("image/png");
-            console.log("Captured Image Data:", imageData); // 캡처된 이미지 데이터 확인
-            setMapImage(imageData);
-        }).catch((error) => {
-            console.error("Error capturing canvas:", error);
-        });
-    };
+            const ctx = canvas.getContext("2d");
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+            ctx.beginPath();
+            polylinePath.forEach((point, index) => {
+                const { Ma, La } = point;
+                if (index === 0) {
+                    ctx.moveTo(La, Ma);
+                } else {
+                    ctx.lineTo(La, Ma);
+                }
+            });
+            ctx.strokeStyle = "#FF0000";
+            ctx.lineWidth = 2;
+            ctx.stroke();
+        
+            html2canvas(canvas, { useCORS: true }).then((canvas) => {
+                const imageData = canvas.toDataURL("image/png");
+                console.log("Captured Image Data:", imageData); // 캡처된 이미지 데이터 확인
+                setMapImage(imageData);
+            }).catch((error) => {
+                console.error("Error capturing canvas:", error);
+            });
+        }, 500); // 500ms 지연
+    };    
 
     // 산책 종료
     const stopTracking = () => {
