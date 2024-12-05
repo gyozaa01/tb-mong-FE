6fb63594e03b082b7be1b27c773e606829c488d9
gyozaa01 <olivia110321@gmail.com>
2024-12-06 01:49:05 +0900

    feat: api.jsx headers 제거

--- src/Pages/Api.jsx
+++ src/Pages/Api.jsx
@@ -12,7 +12,6 @@ api.interceptors.request.use(
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
-    config.headers['Origin'] = 'https://www.tb-mong.xyz';
     return config;
   },
   (error) => Promise.reject(error)
