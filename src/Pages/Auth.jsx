b02cb813d965ccc9881eb431fe3e7cfa5c17584f
gyozaa01 <olivia110321@gmail.com>
2024-12-06 04:32:07 +0900

    feat: auth.jsx 로직 수정

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -18,9 +18,8 @@ const Auth = () => {
           params: { kakaoAccessToken },
         });
     
-        if (response.data && response.data.token) {
-          // 서버에서 JWT 토큰이 반환되는 경우 저장
-          sessionStorage.setItem('jwt_token', response.data.token); // 정확한 필드로 저장
+        if (response.data) {
+          sessionStorage.setItem('jwt_token', response.data);
           navigate('/home'); // 홈 화면으로 이동
         } else {
           console.error('JWT 토큰이 반환되지 않았습니다:', response.data);
