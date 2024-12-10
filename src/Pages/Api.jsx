e8ff58d731386de3be2c5517d2b4344960b6263d
gyozaa01 <olivia110321@gmail.com>
2024-12-10 13:24:17 +0900

    feat: 서버배포된걸로 되돌리기

--- src/Pages/Api.jsx
+++ src/Pages/Api.jsx
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
-// const api = axios.create({
-//   baseURL: 'https://api.tb-mong.xyz', // 상대 경로로 설정
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   withCredentials: true, // 세션 쿠키를 포함하기 위해 설정
-// });
-
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
-  withCredentials: true,
+  baseURL: 'https://api.tb-mong.xyz', // 상대 경로로 설정
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true, // 세션 쿠키를 포함하기 위해 설정
 });
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:8080',
+//   withCredentials: true,
+// });
 
 // 요청 인터셉터
 api.interceptors.request.use(
