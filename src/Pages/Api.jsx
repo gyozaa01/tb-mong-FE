bf7039574d9e438b8be4391d501621e5041e1b2c
gyozaa01 <olivia110321@gmail.com>
2024-12-11 17:56:01 +0900

    feat: api headers 삭제

--- src/Pages/Api.jsx
+++ src/Pages/Api.jsx
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://api.tb-mong.xyz', // 상대 경로로 설정
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  // headers: {
+  //   'Content-Type': 'application/json',
+  // },
   withCredentials: true, // 세션 쿠키를 포함하기 위해 설정
 });
 
